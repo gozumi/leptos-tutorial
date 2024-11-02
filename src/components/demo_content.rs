1f0db2fd0af6123ab9981f90e91a07e10c681f9c
Ian Anderson <ian.kirk.anderson@gmail.com>
2024-11-02 18:06:34 +0000

    Add dynamic attributes.

--- src/components/demo_content.rs
+++ src/components/demo_content.rs
@@ -5,14 +5,17 @@ pub fn DemoContent() -> impl IntoView {
   let (count, set_count) = create_signal(0);
 
   view! {
-    <button
-      on:click=move |_| {
-        set_count.update(|n| *n += 1);
-      }
-      class=("button-20", move || count() % 2 == 1)
-    >
-      "Click me: "
-      {move || count()}
-    </button>
+    <>
+      <button
+        on:click=move |_| {
+          set_count.update(|n| *n += 1);
+        }
+        class=("button-20", move || count() % 2 == 1)
+      >
+        "Click me: "
+        {move || count()}
+      </button>
+      <progress max=50 value=count />
+    </>
   }
 }
