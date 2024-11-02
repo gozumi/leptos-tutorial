79c10ab1b870c496e98fbd9188d0046b590bc519
Ian Anderson <ian.kirk.anderson@gmail.com>
2024-11-02 18:22:57 +0000

    Add a derived signal.

--- src/components/demo_content.rs
+++ src/components/demo_content.rs
@@ -3,9 +3,10 @@ use leptos::{component, create_signal, view, IntoView, SignalUpdate};
 #[component]
 pub fn DemoContent() -> impl IntoView {
   let (count, set_count) = create_signal(0);
+  let double_count = move || count() * 2;
 
   view! {
-    <>
+    <div class="demo-content">
       <button
         on:click=move |_| {
           set_count.update(|n| *n += 1);
@@ -15,7 +16,8 @@ pub fn DemoContent() -> impl IntoView {
         "Click me: "
         {move || count()}
       </button>
-      <progress max=50 value=count />
-    </>
+      <progress max=50 value=double_count />
+      <p>"Double Count: "{double_count}</p>
+    </div>
   }
 }
