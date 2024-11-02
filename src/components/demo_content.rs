83366cd01e39767db8f775f2aeb84bd481530069
Ian Anderson <ian.kirk.anderson@gmail.com>
2024-11-02 17:42:07 +0000

    Add css styling.

--- src/components/demo_content.rs
+++ src/components/demo_content.rs
@@ -1,12 +1,18 @@
-use leptos::{component, create_signal, view, IntoView};
+use leptos::{component, create_signal, view, IntoView, SignalUpdate};
 
 #[component]
 pub fn DemoContent() -> impl IntoView {
   let (count, set_count) = create_signal(0);
 
   view! {
-    <button on:click=move |_| {
-      set_count(count() + 4);
-    }>"Click me: " {move || count()}</button>
+    <button
+      on:click=move |_| {
+        set_count.update(|n| *n += 1);
+      }
+      class:red=move || count() % 2 == 1
+    >
+      "Click me: "
+      {move || count()}
+    </button>
   }
 }
